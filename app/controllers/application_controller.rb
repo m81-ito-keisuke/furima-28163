ac830d0335f88aa840722d232483ba0a0105d7a0
sia-yuki-f717 <larc_bump717kei@yahoo.co.jp>
2020-11-11 21:14:35 +0900

    バリデーションの設定、サインイン画面の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :last_name, :first_name, :last_kana, :first_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 end
