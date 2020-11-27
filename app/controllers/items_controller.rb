ada790c36a38be329c1f738e4970bbccf7c9fc20
sia-yuki-f717 <larc_bump717kei@yahoo.co.jp>
2020-11-27 13:17:08 +0900

    rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,11 +27,10 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-    redirect_to item_path
+      redirect_to item_path
     else
       render :edit
     end
-
   end
 
   private
