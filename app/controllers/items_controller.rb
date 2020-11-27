b9773d2f0e9ab861bc3c619477c78d902af33234
sia-yuki-f717 <larc_bump717kei@yahoo.co.jp>
2020-11-27 15:04:27 +0900

    出品者でない場合トップページに戻る記述の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if user_signed_in? && current_user.id != @item.user_id
+      redirect_to :index
+    end
+    
     if @item.destroy
     redirect_to root_path
     end
