567c16e449fffa2c43503284d78f88c7668bb1cd
sia-yuki-f717 <larc_bump717kei@yahoo.co.jp>
2020-12-01 23:05:55 +0900

    カラム設定とマイギュレーション

--- db/migrate/20201201133450_create_customers.rb
+++ db/migrate/20201201133450_create_customers.rb
@@ -1,7 +1,8 @@
 class CreateCustomers < ActiveRecord::Migration[6.0]
   def change
     create_table :customers do |t|
-
+      t.integer :user_id,               null: false, foreign_key: true
+      t.integer :item_id,               null: false, foreign_key: true
       t.timestamps
     end
   end
