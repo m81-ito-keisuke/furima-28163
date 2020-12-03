695b93fa95cc5037f21c33c321305a2d7920ab6d
sia-yuki-f717 <larc_bump717kei@yahoo.co.jp>
2020-12-03 21:38:22 +0900

    フォームオブジェクトの実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,25 @@
 class CustomersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    @customer_address = CustomerAddress.new
+  end
+
+  def create
+    @customer_address = CustomerAddress.new(customer_params)
+    if @customer_address.valid?
+    @customer_address.save
+    end
+    render :index
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :customer_id).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
--- app/models/customer_address.rb
+++ app/models/customer_address.rb
@@ -18,7 +18,7 @@ class CustomerAddress
   end
 
   def save
-    Customer.create(item_id: @item.id, user_id: @user.id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_bumber: phone_number, customer_id: @customer.id )
+    Customer.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, customer_id: customer_id )
   end
 end
